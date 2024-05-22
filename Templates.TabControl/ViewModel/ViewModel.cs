198bfb2c4865ce1ceb23f0b342239f8258415ae3
cheffcook <150153672+cheffcook@users.noreply.github.com>
2024-05-22 14:51:30 +0200

    Add files via upload

---
+++ Templates.TabControl/ViewModel/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Templates.TabControl.ViewModel
+{
+    public class TabControlViewModel
+    {
+        private readonly ObservableCollection<ProcessVariablesGroup> _processVariablesGroups;
+        public ObservableCollection<ProcessVariablesGroup> ProcessVariablesGroups => _processVariablesGroups;
+
+        public TabControlViewModel()
+        {
+            _processVariablesGroups = new ObservableCollection<ProcessVariablesGroup>
+            {
+                new ProcessVariablesGroup { Name = "P1A", IsActive = true, ProcessVariables = new ObservableCollection<string>() { "P1A1", "P1A2", "P1A3" } },
+                new ProcessVariablesGroup { Name = "P1P", IsActive = false, ProcessVariables = new ObservableCollection<string>() { "P1P1", "P1P2", "P1P3" } },
+                new ProcessVariablesGroup { Name = "P2A", IsActive = true, ProcessVariables = new ObservableCollection<string>() { "P2A1", "P2A2", "P2A3" } },
+                new ProcessVariablesGroup { Name = "P2P", IsActive = false, ProcessVariables = new ObservableCollection<string>() { "P2P1", "P2P2", "P2P3" } },
+                new ProcessVariablesGroup { Name = "P3A", IsActive = true, ProcessVariables = new ObservableCollection<string>() { "P3A1", "P3A2", "P3A3" } },
+                new ProcessVariablesGroup { Name = "P3P", IsActive = false, ProcessVariables = new ObservableCollection<string>() { "P3P1", "P3P2", "P3P3" } }
+            };
+        }
+    }
+}
---
+++ Templates.TabControl/ViewModel/ViewModelDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Templates.TabControl.ViewModel
+{
+    public class ProcessVariablesGroup
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool IsActive { get; set; }
+        public ObservableCollection<string> ProcessVariables { get; set; } = new ObservableCollection<string>();
+    }
+}
